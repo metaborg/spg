bc4c11ab5e162a7c09f9143d06409f39f5425975
Martijn Dwars <ikben@martijndwars.nl>
2016-05-10 16:37:38 +0200

    Fix many bugs and extend many features
    * Add non-determinism
    * Optimize solving by first processing TypeOf/TypeEquals
    * Add Eq/Diseq naming conditions to resolution
    * Solve AssocConstraint

--- src/main/scala/nl/tudelft/fragments/Solver.scala
+++ src/main/scala/nl/tudelft/fragments/Solver.scala
@@ -6,23 +6,27 @@ object Solver {
   import Graph._
 
   // Solve one constraint
-  def solve(c: Constraint, cs: List[Constraint], all: List[Constraint], ts: List[Type]): Option[Substitution] = c match {
-    case TypeOf(_, t@TypeVar(_)) =>
-      for (ty <- ts) {
-        val result = solve(cs.substituteType(Map(t -> ty)), all, ts)
-
-        if (result.isDefined) {
-          return Some((result.get._1 ++ Map(t -> ty), result.get._2, result.get._3))
-        }
+  def solve(c: Constraint, cs: List[Constraint], all: List[Constraint], ts: TypeEnv): Option[Substitution] = c match {
+    case TypeOf(n@SymbolicName(_), t) =>
+      if (ts.contains(n)) {
+        solve(TypeEquals(ts(n), t) :: cs, all, ts)
+      } else {
+        solve(cs, all, ts + (n -> t))
       }
-
-      None
     case TypeEquals(t1, t2) =>
-      for (u <- t1.unify(t2); r <- solve(cs.substituteType(u), all, ts))
-        yield (r._1 ++ u, r._2, r._3)
-    case Res(n1, n2) =>
+      for (
+        u <- t1.unify(t2);
+        r <- solve(
+          cs.substituteType(u._1).substituteName(u._2),
+          all.substituteType(u._1).substituteName(u._2),
+          ts.substituteType(u._1)
+        )
+      )
+      yield (r._1 ++ u._1, r._2 ++ u._2, r._3)
+    // TODO: stable graph thingy
+    case Res(n1@SymbolicName(_), n2@NameVar(_)) =>
       for ((_, dec, cond) <- Random.shuffle(resolves(n1, all))) {
-        val result = solve(cs.substituteName(Map(n2 -> dec)), all, ts)
+        val result = solve(cs.substituteName(Map[NameVar, Name](n2 -> dec)), all.substituteName(Map(n2 -> dec)), ts)
 
         if (result.isDefined) {
           return Some((result.get._1, result.get._2 ++ Map(n2 -> dec), result.get._3 ++ cond))
@@ -30,19 +34,69 @@ object Solver {
       }
 
       None
+    case AssocConstraint(n@SymbolicName(_), s@ScopeVar(_)) =>
+      val scope = associated(n, all)
+
+      if (scope.isDefined) {
+        solve(cs.substituteScope(Map(s -> scope.get)), all.substituteScope(Map(s -> scope.get)), ts)
+      } else {
+        None
+      }
     case _ =>
-      solve(cs, all, ts)
+      None
   }
 
-  // Solve all constraints
-  def solve(C: List[Constraint], all: List[Constraint], ts: List[Type]): Option[Substitution] = C match {
-    case x :: xs => solve(x, xs, all, ts)
-    case Nil => Some(Map[TypeVar, Type](), Map[NameVar, NameVar](), List.empty)
-  }
+  // Solve all constraints non-deterministically, but give TypeEquals and TypeOf(SymbolicName(_), _) precedence
+  def solve(C: List[Constraint], all: List[Constraint], ts: TypeEnv): Option[Substitution] =
+    if (C.nonEmpty) {
+      if (C.exists(c => c.isInstanceOf[TypeOf] && c.asInstanceOf[TypeOf].n.isInstanceOf[SymbolicName])) {
+        val c = C.find(c => c.isInstanceOf[TypeOf] && c.asInstanceOf[TypeOf].n.isInstanceOf[SymbolicName]).get
+        val result = solve(c, C-c, all, ts)
+
+        if (result.isDefined) {
+          return result
+        }
+      } else if (C.exists(_.isInstanceOf[TypeEquals])) {
+        val c = C.find(_.isInstanceOf[TypeEquals]).get
+        val result = solve(c, C-c, all, ts)
+
+        if (result.isDefined) {
+          return result
+        }
+      } else {
+        for (c <- C) {
+          val result = solve(c, C - c, all, ts)
+
+          if (result.isDefined) {
+            return result
+          }
+        }
+      }
+
+      None
+    } else {
+      Some(Map[TypeVar, Type](), Map[NameVar, NameVar](), List.empty)
+    }
 
   // Solve all constraints
-  def solve(constraints: List[Constraint], ts: List[Type]): Option[Substitution] =
-    solve(constraints, constraints, ts)
+  def solve(constraints: List[Constraint]): Option[Substitution] =
+    solve(constraints.filter(_.isProper), constraints, TypeEnv())
+}
+
+case class TypeEnv(bindings: Map[SymbolicName, Type] = Map.empty) {
+  def contains(n: SymbolicName): Boolean =
+    bindings.contains(n)
+
+  def apply(n: SymbolicName) =
+    bindings(n)
+
+  def +(e: (SymbolicName, Type)) =
+    TypeEnv(bindings + e)
+
+  def substituteType(typeBinding: TypeBinding) =
+    TypeEnv(bindings.map { case (nameVar, typ) =>
+      nameVar -> typ.substituteType(typeBinding)
+    })
 }
 
 object Graph {
@@ -56,16 +110,16 @@ object Graph {
     }
 
   // Declarations
-  def declarations(s: Scope, all: List[Constraint]): List[NameVar] =
+  def declarations(s: Scope, all: List[Constraint]): List[SymbolicName] =
     all.flatMap {
-      case Dec(`s`, n) =>
+      case Dec(`s`, n@SymbolicName(_)) =>
         Some(n)
       case _ =>
         None
     }
 
   // Scope for name
-  def scope(n: NameVar, all: List[Constraint]): List[Scope] =
+  def scope(n: SymbolicName, all: List[Constraint]): List[Scope] =
     all.flatMap {
       case Dec(s, `n`) =>
         Some(s)
@@ -75,20 +129,48 @@ object Graph {
         None
     }
 
-  // Reachable scopes
-  def path(s: Scope, all: List[Constraint]): List[(Path, Scope)] =
-    List((Nil, s)) ++ parent(s, all).flatMap(path(_, all)).map { case (path, scope) =>
-      (s :: path, scope)
+  // Scope associated with a declaration
+  def associated(n: SymbolicName, all: List[Constraint]): Option[Scope] =
+    all.flatMap {
+      case AssocFact(`n`, s) =>
+        Some(s)
+      case _ =>
+        None
+    }.headOption
+
+  // Direct imports
+  def dimports(s: Scope, all: List[Constraint]): List[Scope] =
+    all.flatMap {
+      case DirectImport(`s`, s2) =>
+        Some(s2)
+      case _ =>
+        None
     }
 
+  // Parent edge
+  def edgeParent(s: Scope, all: List[Constraint]): List[(Path, Scope)] =
+    parent(s, all).flatMap(path(_, all)).map { case (path, scope) =>
+      (Parent() :: path, scope)
+    }
+
+  // Direct Import edge
+  def edgeDImport(s: Scope, all: List[Constraint]): List[(Path, Scope)] =
+    dimports(s, all).flatMap(imports => path(imports, all).map((imports, _))).map { case (imports, (path, scope)) =>
+      (Import(imports) :: path, scope)
+    }
+
+  // Transitive reflexive closure of edge relation // TODO: Include named imports
+  def path(s: Scope, all: List[Constraint]): List[(Path, Scope)] =
+    List((Nil, s)) ++ edgeParent(s, all) ++ edgeDImport(s, all)
+
   // Reachable declarations
-  def reachable(s: Scope, all: List[Constraint]): List[(Path, NameVar)] =
+  def reachable(s: Scope, all: List[Constraint]): List[(Path, SymbolicName)] =
     path(s, all).flatMap { case (path, scope) =>
       declarations(scope, all).map(dec => (path, dec))
     }
 
   // Visible declarations
-  def visible(s: Scope, all: List[Constraint]): List[(Path, NameVar, List[Diseq])] =
+  def visible(s: Scope, all: List[Constraint]): List[(Path, SymbolicName, List[Condition])] =
     reachable(s, all).map { case (path, name) =>
       val condition = reachable(s, all)
         .filter(_._1.length < path.length)
@@ -98,8 +180,22 @@ object Graph {
     }
 
   // Resolution
-  def resolves(n: NameVar, all: List[Constraint]): List[(Path, NameVar, List[Diseq])] =
-    scope(n, all).flatMap(visible(_, all))
+  def resolves(n: SymbolicName, all: List[Constraint]): List[(Path, SymbolicName, List[Condition])] =
+    scope(n, all).flatMap(s =>
+      visible(s, all).map { case (path, dec, cs) =>
+        (path, dec, Eq(n, dec) :: cs)
+      }
+    )
 }
 
-case class Diseq(n1: NameVar, n2: NameVar)
+abstract class PathElem
+case class Declaration(n: NameVar) extends PathElem
+case class Parent() extends PathElem
+case class Import(s: Scope) extends PathElem
+
+abstract class Condition
+case class Diseq(n1: SymbolicName, n2: SymbolicName) extends Condition
+case class Eq(n1: Name, n2: Name) extends Condition {
+  def substituteConcrete(binding: ConcreteBinding) =
+    Eq(n1.substituteConcrete(binding), n2.substituteConcrete(binding))
+}
