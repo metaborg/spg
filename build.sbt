9fb708a76315a5d7964e18aa4fb25a28d4cf781e
Martijn Dwars <ikben@martijndwars.nl>
2016-05-10 16:37:07 +0200

    Add Metaborg and conversion to Stratego terms

--- build.sbt
+++ build.sbt
@@ -4,4 +4,12 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
+resolvers += "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository-new"
+
 libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.6" % "test"
+
+libraryDependencies += "org.metaborg" % "org.metaborg.core" % "2.0.0-SNAPSHOT"
+
+libraryDependencies += "org.metaborg" % "org.metaborg.util" % "2.0.0-SNAPSHOT"
+
+libraryDependencies += "org.metaborg" % "org.metaborg.spoofax.core" % "2.0.0-SNAPSHOT"
--- src/main/scala/nl/tudelft/fragments/Concretor.scala
+++ src/main/scala/nl/tudelft/fragments/Concretor.scala
@@ -1,12 +1,55 @@
 package nl.tudelft.fragments
 
 object Concretor {
-  // Concretize the rule's pattern by assigning concrete names and types
-  def concretize(rule: Rule, diseq: List[Diseq]): Pattern = {
-    // val names = rule.pattern.names
+  // Concretize the rule by a) choosing a substitution (e.g. there may be many solutions = substitutions) and b) assigning concrete names and types
+  def concretize(rule: Rule, solution: Substitution): Pattern = {
+    // Apply solution (substitution) to the pattern
+    val solved = rule.pattern
+      .substituteType(solution._1)
+      .substituteName(solution._2)
 
-    // TODO: Here you can choose for as few distinct names (satisfying the disequality constraints) or as many as possible or anywhere in between
+    // Get all names
+    val names = solved.names
 
-    ???
+    val eqs = solution._3.flatMap {
+      case x: Eq =>
+        Some(x)
+      case _ =>
+        None
+    }
+
+    // 2. Assign different names to names in Diseq conditions
+    // 3. Assign names to the remaining SymbolicNames by randomly either
+    //   a) choosing an existing name or
+    //   b) choosing a new name.
+
+    solved
+      .substituteConcrete(name(eqs, NameProvider(0)))
+  }
+
+  // Generate a binding from symbolcic names to concrete names satisfying the equality constraints
+  def name(eqs: List[Eq], nameProvider: NameProvider): ConcreteBinding = eqs match {
+    case Eq(SymbolicName(n1), ConcreteName(n2)) :: _ =>
+      val binding = Map(SymbolicName(n1) -> ConcreteName(n2))
+
+      name(eqs.substituteConcrete(binding), nameProvider) ++ binding
+    case Eq(ConcreteName(n2), SymbolicName(n1)) :: _ =>
+      val binding = Map(SymbolicName(n1) -> ConcreteName(n2))
+
+      name(eqs.substituteConcrete(binding), nameProvider) ++ binding
+    case Eq(SymbolicName(n), _) :: _ =>
+      val next = "n" + nameProvider.next
+      val binding = Map(SymbolicName(n) -> ConcreteName(next))
+
+      name(eqs.substituteConcrete(binding), nameProvider) ++ binding
+    case Eq(_, SymbolicName(n)) :: _ =>
+      val next = "n" + nameProvider.next
+      val binding = Map(SymbolicName(n) -> ConcreteName(next))
+
+      name(eqs.substituteConcrete(binding), nameProvider) ++ binding
+    case _ :: tail =>
+      name(tail, nameProvider)
+    case Nil =>
+      Map.empty
   }
 }
