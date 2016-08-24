4cde3396d2d3a520a0a694412035f87252db6f58
Martijn Dwars <ikben@martijndwars.nl>
2016-08-24 12:48:45 +0200

    Move injection computation and use unification
    Use unification instead of equality such that we can compute injections
    for parametric sorts such as IterStar(a).

--- src/main/scala/nl/tudelft/fragments/spoofax/models/Symbol.scala
+++ src/main/scala/nl/tudelft/fragments/spoofax/models/Symbol.scala
@@ -1,9 +1,73 @@
 package nl.tudelft.fragments.spoofax.models
 
+import nl.tudelft.fragments._
+
 abstract class Symbol
 
-// Sort usage
-case class Sort(name: String) extends Symbol
+// Sort
+abstract class Sort extends Symbol {
+  def substituteSort(binding: Map[SortVar, Sort]): Sort
+
+  def unify(sort: Sort, binding: Map[SortVar, Sort] = Map.empty): Option[Map[SortVar, Sort]]
+}
+
+object Sort {
+  def injections(signatures: List[Signature])(sort: Sort): Set[Sort] = {
+    val sorts = signatures.flatMap {
+      case OpDeclInj(FunType(List(ConstType(x)), ConstType(y))) =>
+        y.unify(sort).map(x.substituteSort)
+      case _ =>
+        Option.empty[Sort]
+    }
+
+    sorts.toSet
+  }
+
+  def transitiveInjections(signatures: List[Signature])(sorts: Set[Sort]): Set[Sort] =
+    sorts.flatMap(injections(signatures)) ++ sorts
+
+  def injectionsClosure(signatures: List[Signature])(sorts: Set[Sort]): Set[Sort] =
+    fixedPoint(transitiveInjections(signatures), sorts)
+}
+
+case class SortAppl(name: String, children: List[Sort] = Nil) extends Sort {
+  override def substituteSort(binding: Map[SortVar, Sort]): Sort =
+    SortAppl(name, children.map(_.substituteSort(binding)))
+
+  override def unify(sort: Sort, binding: Map[SortVar, Sort]): Option[Map[SortVar, Sort]] = sort match {
+    case c@SortAppl(`name`, _) if children.length == c.children.length =>
+      children.zip(c.children).foldLeftWhile(binding) {
+        case (binding, (t1, t2)) =>
+          t1.unify(t2, binding)
+      }
+    case SortVar(_) =>
+      sort.unify(this, binding)
+    case _ =>
+      None
+  }
+
+  override def toString: String =
+    s"""SortAppl("$name", $children)"""
+}
+
+case class SortVar(name: String) extends Sort {
+  override def substituteSort(binding: Map[SortVar, Sort]): Sort =
+    binding.getOrElse(this, this)
+
+  override def unify(sort: Sort, binding: Map[SortVar, Sort]): Option[Map[SortVar, Sort]] = sort match {
+    case s@SortVar(_) if binding.contains(s) =>
+      unify(binding(s), binding)
+    case _ =>
+      if (binding.contains(this)) {
+        binding(this).unify(sort, binding)
+      } else {
+        Some(binding + (this -> sort))
+      }
+  }
+
+  override def toString: String =
+    s"""SortVar("$name")"""
+}
 
 // Literal string
 case class Lit(text: String) extends Symbol
