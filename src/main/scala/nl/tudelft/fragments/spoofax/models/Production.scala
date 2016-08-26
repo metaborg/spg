489492f7741d7817f8af525941d28aee800af7b7
Martijn Dwars <ikben@martijndwars.nl>
2016-08-26 10:46:10 +0200

    Add production-to-signature conversion

--- src/main/scala/nl/tudelft/fragments/spoofax/models/Production.scala
+++ src/main/scala/nl/tudelft/fragments/spoofax/models/Production.scala
@@ -1,7 +1,29 @@
 package nl.tudelft.fragments.spoofax.models
 
-abstract class Prod
+case class Production(sort: Sort, rhs: List[Symbol], cons: Option[String] = None) {
+  def toSignature: Signature = {
+    def rhsToConstType(rhs: Symbol): Option[ConstType] = rhs match {
+      case x@SortAppl(_, _) =>
+        Some(ConstType(x))
+      case Iter(sort: Sort) =>
+        Some(ConstType(SortAppl("Iter", List(sort))))
+      case IterStar(sort: Sort) =>
+        Some(ConstType(SortAppl("IterStar", List(sort))))
+      case IterSep(sort: Sort, _) =>
+        Some(ConstType(SortAppl("Iter", List(sort))))
+      case IterStarSep(sort: Sort, _) =>
+        Some(ConstType(SortAppl("IterStar", List(sort))))
+      case Opt(sort: Sort) =>
+        Some(ConstType(SortAppl("Option", List(sort))))
+      case _ =>
+        None
+    }
 
-case class Production(sort: Sort, rhs: List[Symbol]) extends Prod
-
-case class ProductionWithCons(sort: Sort, rhs: List[Symbol], cons: String) extends Prod
+    cons match {
+      case Some(name) =>
+        OpDecl(name, FunType(rhs.flatMap(rhsToConstType), ConstType(sort)))
+      case None =>
+        OpDeclInj(FunType(rhs.flatMap(rhsToConstType), ConstType(sort)))
+    }
+  }
+}
