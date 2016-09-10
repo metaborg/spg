f5adb98145e1d96dde7c5c3722abddce1a50597e
Martijn Dwars <ikben@martijndwars.nl>
2016-09-10 16:18:55 +0200

    Different sorts continuations

--- src/main/scala/nl/tudelft/fragments/Check.scala
+++ src/main/scala/nl/tudelft/fragments/Check.scala
@@ -2,6 +2,7 @@ package nl.tudelft.fragments
 
 import nl.tudelft.fragments.Check.Path
 import nl.tudelft.fragments.regex.Regex
+import nl.tudelft.fragments.spoofax.models.Sort
 
 object Check {
   type Path = List[Label]
@@ -28,11 +29,11 @@ object Check {
     * @param c
     * @return
     */
-  def declarationability(rws: List[Rewrite], c: Regex[Label]): Boolean =
-    declarationability(rws, c, Set.empty)
+  def declarationability(rws: List[Rewrite], g: List[Constraint], s: Scope, c: Regex[Label]): Boolean =
+    declarationability(rws, g, s, c, Set.empty)
 
   /**
-    * Determine whether we can add a declaration given continuation c and seen
+    * Determine whether we can add a declaration that is reachable from scope s with continuation c and seen
     * continuations cs.
     *
     * @param rws Possible rewrites
@@ -40,20 +41,32 @@ object Check {
     * @param cs  Set of seen continuations
     * @return
     */
-  def declarationability(rws: List[Rewrite], c: Regex[Label], cs: Set[Regex[Label]]): Boolean = {
-    for (rewrite <- rws) {
-      for ((path, scope) <- paths(rewrite)) {
-        val nc = c.derive(path)
+  def declarationability(rws: List[Rewrite], g: List[Constraint], s: Scope, c: Regex[Label], cs: Set[(Sort, Regex[Label])]): Boolean = {
+    val recurses = g.collect { case x: CGenRecurse => x }
 
-        if (!nc.acceptsEmptyString) {
-          if (!cs.contains(nc)) {
-            if (declarationability(rws, nc, cs + nc)) {
+    for (recurse <- recurses) {
+      val rewrites = rws.filter(rewrite => recurse.sort == rewrite.sort)
+
+      for (rewrite <- rewrites) {
+        val ng = applyRewrite(g, recurse, rewrite)
+
+        for ((path, scope) <- paths(s, ng)) {
+          val nc = c.derive(path)
+
+          if (nc.acceptsEmptyString) {
+            if (decls(ng, scope).nonEmpty) {
               return true
             }
           }
-        } else {
-          if (decls(rewrite, scope).nonEmpty) {
-            return true
+
+          val sorts = sortsForScope(ng, scope)
+
+          for (sort <- sorts) {
+            if (!cs.contains((sort, nc))) {
+              if (declarationability(rws, ng, scope, nc, cs + (sort -> nc))) {
+                return true
+              }
+            }
           }
         }
       }
@@ -63,22 +76,57 @@ object Check {
   }
 
   /**
-    * All paths in the rewrite from the source scope
+    * Get the sorts of the recurses for the given scope s in the graph g.
     *
-    * @param rw
+    * @param g
+    * @param s
     * @return
     */
-  def paths(rw: Rewrite): List[(Path, Scope)] =
-    Graphx(rw.constraints).path(ScopeAppl("s"))
+  def sortsForScope(g: List[Constraint], s: Scope): List[Sort] = g
+    .collect { case x: CGenRecurse => x }
+    .filter(_.scopes.head == s)
+    .map(_.sort)
 
   /**
-    * All declarations in the scope s in the rewrite rw
+    * Apply rewrite r to scope recurse.scopes.head in graph g and remove the
+    * recurse constraint from g.
     *
-    * @param rw
+    * @param g
+    * @param recurse
     * @return
     */
-  def decls(rw: Rewrite, s: Scope): List[Pattern] =
-    Graphx(rw.constraints).decls(s)
+  def applyRewrite(g: List[Constraint], recurse: CGenRecurse, r: Rewrite): List[Constraint] = {
+    // Freshen names in the rewrite
+    val (_, freshr) = r.freshen()
+
+    // Unify s with ScopeAppl("s") in the rewrite (by convention)
+    val unifier = freshr.scope.unify(recurse.scopes.head).get
+
+    // Propagate the unification
+    val substituted = freshr.substitute(unifier)
+
+    // Merge g with r
+    (g - recurse) ++ substituted.constraints
+  }
+
+  /**
+    * All paths in the graph from scope s
+    *
+    * @param s
+    * @param g
+    * @return
+    */
+  def paths(s: Scope, g: List[Constraint]): List[(Path, Scope)] =
+    (Nil, s) :: Graphx(g).path(s)
+
+  /**
+    * All declarations in the scope s in the graph g
+    *
+    * @param g
+    * @return
+    */
+  def decls(g: List[Constraint], s: Scope): List[Pattern] =
+    Graphx(g).decls(s)
 }
 
 case class Graphx(constraints: List[Constraint]) {
@@ -107,5 +155,14 @@ case class Graphx(constraints: List[Constraint]) {
     path(s).flatMap { case (_, ss) => decls(ss) }
 }
 
-case class Rewrite(constraints: List[Constraint])
+case class Rewrite(sort: Sort, scope: Scope, constraints: List[Constraint]) {
+  def freshen(nameBinding: Map[String, String] = Map.empty): (Map[String, String], Rewrite) =
+    constraints.freshen(nameBinding).map { case (nameBinding, constraints) =>
+      scope.freshen(nameBinding).map { case (nameBinding, scope) =>
+        (nameBinding, Rewrite(sort, scope, constraints))
+      }
+    }
 
+  def substitute(binding: ScopeBinding): Rewrite =
+    Rewrite(sort, scope.substituteScope(binding), constraints.substituteScope(binding))
+}
--- src/test/scala/nl/tudelft/fragments/CheckSuite.scala
+++ src/test/scala/nl/tudelft/fragments/CheckSuite.scala
@@ -1,28 +1,37 @@
 package nl.tudelft.fragments
 
-import nl.tudelft.fragments.regex.Character
+import nl.tudelft.fragments.LabelImplicits._
+import nl.tudelft.fragments.spoofax.models.SortAppl
 import org.scalatest.FunSuite
 
 class CheckSuite extends FunSuite {
   test("even Ps will succeed") {
     // Single rewrite
     val R = List(
-      Rewrite(List(
+      Rewrite(SortAppl("VarDecl"), ScopeAppl("s"), List(
         CGDirectEdge(ScopeAppl("s"), Label('P'), ScopeAppl("s2")),
         CGDecl(ScopeAppl("s2"), SymbolicName("Class", "x"))
       ))
     )
 
     // Any number of Ps: P, PP, PPP, ...
-    val c = Character('P') *
+    val c = Label('P') *
 
-    assert(Check.declarationability(R, c))
+    // Initial graph
+    val g = List(
+      CGenRecurse(TermVar("x"), List(ScopeAppl("s")), None, SortAppl("VarDecl"))
+    )
+
+    // Scope for which the continuation applies
+    val s = ScopeAppl("s")
+
+    assert(Check.declarationability(R, g, s, c))
   }
 
   test("uneven Ps will terminate and fail") {
     // Single rewrite
     val R = List(
-      Rewrite(List(
+      Rewrite(SortAppl("VarDecl"), ScopeAppl("s"), List(
         CGDirectEdge(ScopeAppl("s"), Label('P'), ScopeAppl("s2")),
         CGDirectEdge(ScopeAppl("s2"), Label('P'), ScopeAppl("s3")),
         CGDecl(ScopeAppl("s3"), SymbolicName("Class", "x"))
@@ -30,8 +39,82 @@ class CheckSuite extends FunSuite {
     )
 
     // Uneven number of Ps: P, PPP, PPPPP, ...
-    val c = Character('P') ~ ((Character('P') ~ Character('P')) *)
+    val c = Label('P') ~ ((Label('P') ~ Label('P')) *)
 
-    assert(!Check.declarationability(R, c))
+    // Initial graph
+    val g = List(
+      CGenRecurse(TermVar("x"), List(ScopeAppl("s")), None, SortAppl("VarDecl"))
+    )
+
+    // Scope for which the continuation applies
+    val s = ScopeAppl("s")
+
+    assert(!Check.declarationability(R, g, s, c))
+  }
+
+
+  test("sorted rewrites succeeds") {
+    // Single rewrite
+    val R = List(
+      // Class : Field -> ClassDecl
+      Rewrite(SortAppl("ClassDecl"), ScopeAppl("s"), List(
+        CGDirectEdge(ScopeAppl("s"), Label('P'), ScopeAppl("s2")),
+        CGenRecurse(TermVar("x"), List(ScopeAppl("s2")), None, SortAppl("FieldDecl"))
+      )),
+
+      // Field : String -> FieldDecl
+      Rewrite(SortAppl("FieldDecl"), ScopeAppl("s"), List(
+        CGDecl(ScopeAppl("s"), SymbolicName("Ns", "x"))
+      ))
+    )
+
+    // Any number of Ps: P, PP, PPP, ...
+    val c = Label('P')*
+
+    // Initial graph
+    val g = List(
+      CGenRecurse(TermVar("x"), List(ScopeAppl("s")), None, SortAppl("ClassDecl"))
+    )
+
+    // Scope for which the continuation applies
+    val s = ScopeAppl("s")
+
+    assert(Check.declarationability(R, g, s, c))
+  }
+
+  test("continuation from different sorts are different") {
+    // Single rewrite
+    val R = List(
+      // Class : Field -> ClassDecl
+      Rewrite(SortAppl("ClassDecl"), ScopeAppl("s"), List(
+        CGDirectEdge(ScopeAppl("s"), Label('P'), ScopeAppl("s2")),
+        CGenRecurse(TermVar("x"), List(ScopeAppl("s2")), None, SortAppl("FieldDecl"))
+      )),
+
+      // Field : String -> FieldDecl
+      Rewrite(SortAppl("FieldDecl"), ScopeAppl("s"), List(
+        CGDirectEdge(ScopeAppl("s"), Label('P'), ScopeAppl("s2")),
+        CGenRecurse(TermVar("x"), List(ScopeAppl("s2")), None, SortAppl("Exp"))
+      )),
+
+      // Exp : String -> Exp
+      Rewrite(SortAppl("Exp"), ScopeAppl("s"), List(
+        CGDirectEdge(ScopeAppl("s"), Label('P'), ScopeAppl("s2")),
+        CGDecl(ScopeAppl("s"), SymbolicName("Ns", "x"))
+      ))
+    )
+
+    // Any number of Ps: P, PP, PPP, ...
+    val c = Label('P')*
+
+    // Initial graph
+    val g = List(
+      CGenRecurse(TermVar("x"), List(ScopeAppl("s")), None, SortAppl("ClassDecl"))
+    )
+
+    // Scope for which the continuation applies
+    val s = ScopeAppl("s")
+
+    assert(Check.declarationability(R, g, s, c))
   }
 }
